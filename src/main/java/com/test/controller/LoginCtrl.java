3753b9a16d74eddba97c481d1fed53df84fdc949
faaaamine <2821035116@qq.com>
2024-03-26 08:34:56 +0800

    用户加密

--- src/main/java/com/test/controller/LoginCtrl.java
+++ src/main/java/com/test/controller/LoginCtrl.java
@@ -1,26 +1,41 @@
 package com.test.controller;
 
 import com.test.entity.Adminuser;
+import com.test.service.AdminuserService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin")
 public class LoginCtrl {
 
+    @Resource
+    AdminuserService adminuserService;
+
     @GetMapping("/goLogin")
-    public String goLogin() {
-        System.out.println("登录回答");
-        return "login"; // 返回逻辑视图名，框架会自动解析为实际路径
+    public String toLogin() {
+        return "login";
+        //return "../templates/login.html";
     }
 
     @PostMapping("/doLogin")
+    //public String doLogin(String uName,@RequestParam(value = "pwd") String uPwd){
     public String doLogin(Adminuser adminuser) {
-        System.out.println(adminuser.getName() + "===" + adminuser.getPassword());
+        System.out.println(adminuser.getName() + " ===" + adminuser.getPassword());
         System.out.println("登录处理");
-        return "goodsList"; // 返回逻辑视图名，框架会自动解析为实际路径
-    }
-}
 
+        Adminuser user = adminuserService.getAdminuser(adminuser);
+        System.out.println(user);
+
+        if(user== null){
+
+        }
+        return "goodsList";
+        //return "../templates/goodsList.html";
+    }
+
+}
--- src/main/java/com/test/service/Impl/AdminuserServiceImpl.java
+++ src/main/java/com/test/service/Impl/AdminuserServiceImpl.java
@@ -5,6 +5,7 @@ import com.test.entity.Adminuser;
 import com.test.service.AdminuserService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 @Service
 public class AdminuserServiceImpl implements AdminuserService {
@@ -15,6 +16,9 @@ public class AdminuserServiceImpl implements AdminuserService {
     @Override
     public Adminuser getAdminuser(Adminuser adminuser) {
         // 密码加密
+        String psw = DigestUtils.md5DigestAsHex(adminuser.getPassword().getBytes());
+        System.out.println(psw);
+        adminuser.setPassword(psw);
         return adminuserDao.getAdminuser(adminuser);
     }
 }
